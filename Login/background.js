03d6a5950dafdc28d84e7fda52a94d2958c7cf4f
nandaniya480 <nandaniya480@gmail.com>
2025-04-21 23:42:46 +0530

    React plugin

---
+++ Login/background.js
@@ -0,0 +1,4 @@
+// chrome.runtime.onInstalled.addListener(() => {
+//     chrome.tabs.create({ url: "http://192.168.1.200:88/login" });
+//   });
+  
---
+++ my-chrome-extension/src/popup.jsx
@@ -0,0 +1,9 @@
+export default function App() {
+    return (
+      <div style={{ padding: '1rem' }}>
+        <h2>My Extension</h2>
+        <button onClick={() => alert('Login clicked!')}>Login</button>
+      </div>
+    );
+  }
+  
